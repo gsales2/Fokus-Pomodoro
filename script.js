edb38b7b91931266e34050d07455b2ebd49a85cf
gsales2 <gabrielsales2002@gmail.com>
2025-12-11 19:14:11 -0300

    Criada variavel de botao iniciar, variaveis de timer para cada categoria e botao iniciar com if (e console.log para teste)

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
-const timer = document.querySelector('.app__card-timer')
-const imagem = document.querySelector('.app__image')
-const title = document.querySelector('.app__title')
+const botaoIniciar = document.querySelector('.app__card-primary-button')
+const displayTempo = document.querySelector('#timer')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+
+const tempFoco = 1500
+const tempDescansoCurto = 300
+const tempDescansoLongo = 900
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
@@ -16,6 +21,17 @@ curtoBt.addEventListener('click', () => {
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
+})
+
+timerBt.addEventListener('click', () => {
+    const contexto = html.dataset.contexto
+    if(contexto === 'foco') {
+        console.log('foco')
+    }else if (contexto === 'descanso-curto') {
+        console.log('descanso curto')
+    }else {
+        console.log('Descanso longo')
+    }
 })
 
 
